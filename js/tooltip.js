e77f7f0c82cb6e11aabbf25b587b1ab1d49973b5
Andy Hoffman <antibland@gmail.com>
2015-01-25 18:36:19 -0500

    Rewrite tooltip to support focus and hover

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
   $.fn.tooltip = function() {
     return this.each(function() {
-      var $tooltip_message = $("<div class='tooltip_message'></div"),
-          changeTipPosition = function(event) {
-            var xPos = event.pageX - 20,
-                yPos = event.pageY - 45;
+      var $tooltip_message = $("<div class='tooltip-message'></div"),
+          changeTipPosition = function(event, action) {
+            var xPos, yPos, $target;
+
+            if (action === "keyboard") {
+              $target = $(event.target);
+              xPos = $target.offset().left;
+              yPos = $target.offset().top + 25;
+            } else {
+              xPos = event.pageX;
+              yPos = event.pageY;
+            }
+
             $tooltip_message.css({
-              transform: 'translate3d(' + xPos + 'px,' + yPos + 'px, 0)'
+              top: yPos + "px",
+              left: xPos + "px"
             });
           },
-          showTip = function(event) {
-            var tooltip_content = $(this).attr("data-tooltip-content");
+          showTip = function(event, action) {
+            $target = action === "keyboard" ? $(event.target) : $(this);
+            var tooltip_content = $target.attr("data-tooltip-content");
             hideTip();
             setTipContent(tooltip_content);
-            changeTipPosition(event);
+            changeTipPosition(event, action);
           },
           hideTip = function() {
-            $(".tooltip_message").remove();
+            $(".tooltip-message").remove();
           },
           setTipContent = function(tooltip_content) {
             $tooltip_message
@@ -24,10 +35,14 @@ $(document).ready(function() {
               .appendTo('body');
           };
 
-      $(this).bind({
+      $(this).on({
         mousemove: changeTipPosition,
         mouseenter: showTip,
-        mouseleave: hideTip
+        focus: function() {
+          showTip(event, 'keyboard');
+        },
+        mouseleave: hideTip,
+        blur: hideTip
       });
     });
   };
